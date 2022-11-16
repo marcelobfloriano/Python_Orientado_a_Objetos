5a67bbbe672de42646ed585f02162d23c0968cbc
Marcelo Braga Floriano <113947564+marcelobfloriano@users.noreply.github.com>
2022-11-16 06:45:35 -0300

    add

---
+++ conta.py
@@ -0,0 +1,44 @@
+class Conta:
+
+    def __init__(self, numero, titular, saldo=0.0, limite = 1000):
+        self.__numero  = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print(f'O saldo do {self.__titular} é R$ {self.__saldo}')
+
+    def depositar(self, valor):
+        self.__saldo += valor
+        print(f'Depositando R$ {valor} na conta de {self.__titular}')
+        print(f'Novo saldo R$ {self.__saldo}')
+
+    def sacar(self, valor):
+        print(f'Valor do Saque R$ {valor}')
+        if self.__saldo < valor:
+            print('Saldo insuficiente...')
+        else:
+            self.__saldo -= valor
+            print(f'Sacando R$ {valor} na conta de {self.__titular}')
+            print(f'Novo saldo R$ {self.__saldo}')
+    
+    def transferir(self, valor, destino):
+        if valor <= self.__saldo:
+            self.sacar(valor)
+            destino.depositar(valor)
+        else:
+            print('Saldo insuficiente para tranferencia')
+        
+conta = Conta(1, 'marcelo', 90.50)
+conta2 = Conta(2, 'neymar', 1500, 25000)
+
+conta.extrato()
+conta.depositar(500) 
+conta.sacar(600)
+
+conta2.extrato()
+conta2.depositar(500)
+conta2.sacar(350)
+
+conta.transferir(2000, conta2)
