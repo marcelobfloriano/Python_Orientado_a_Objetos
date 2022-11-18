03c08319c5359db4ab54f9b032ef8211152fa86b
Marcelo Braga Floriano <113947564+marcelobfloriano@users.noreply.github.com>
2022-11-18 09:46:22 -0300

    Add

--- conta.py
+++ conta.py
@@ -16,12 +16,12 @@ class Conta:
 
     def sacar(self, valor):
         print(f'Valor do Saque R$ {valor}')
-        if self.__saldo < valor:
+        if (self.__saldo + self.__limite) < valor:
             print('Saldo insuficiente...')
         else:
-            self.__saldo -= valor
+            (self.__saldo + self.__limite) - valor 
             print(f'Sacando R$ {valor} na conta de {self.__titular}')
-            print(f'Novo saldo R$ {self.__saldo}')
+            
     
     def transferir(self, valor, destino):
         if valor <= self.__saldo:
@@ -30,6 +30,27 @@ class Conta:
         else:
             print('Saldo insuficiente para tranferencia')
         
+    
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+
+
+
+
 conta = Conta(1, 'marcelo', 90.50)
 conta2 = Conta(2, 'neymar', 1500, 25000)
 
